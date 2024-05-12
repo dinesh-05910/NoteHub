53862192af277ecd3d5e0996d6eb53feaeceef23
dinesh-05910 <chd.05910@gmail.com>
2024-05-12 16:49:52 +0530

    Initial Commit - NoteHub App

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EditTwoToneIcon />
+        NoteHub
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
